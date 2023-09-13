7b471af64fc44144607df1aa490835bb92e6f7f5
Kamil Zalyalov <kamil.3alyalov@gmail.com>
2023-09-13 15:00:04 +0300

    Initial commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using REST.Models;
+
+namespace REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _dbContext;
+
+        public MoviesController(MovieContext dbContext)
+        {
+            _dbContext = dbContext; ; ;
+            dbContext.SaveChanges();
+        }
+
+        //GET: api/Movies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if (_dbContext.Movies==null)
+                return NotFound();
+
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        //GET: api/Movies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+                return NotFound();
+            var movie = await _dbContext.Movies.FindAsync(id);
+
+            if (movie == null)
+                return NotFound();
+
+            return movie;
+        }
+
+        //POST: api/Movies
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new {id = movie.Id}, movie);
+        }
+
+        //PUT: api/Movies/5
+        [HttpPut]
+        public async Task<ActionResult<Movie>> PutMovie(int id, Movie movie)
+        {
+            if (id != movie.Id)
+                return BadRequest();
+
+            _dbContext.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+                return NotFound();
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+
+            if (movie == null) 
+                return NotFound();
+
+            _dbContext.Movies.Remove(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(long id)
+        {
+            return (_dbContext.Movies?.Any(x => x.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace REST.Models
+{
+    public class MovieContext: DbContext
+    {
+        public DbSet<Movie> Movies { get; set; } = null!;
+
+        public MovieContext(DbContextOptions<MovieContext> options) 
+            : base(options) { }
+
+    }
+}
